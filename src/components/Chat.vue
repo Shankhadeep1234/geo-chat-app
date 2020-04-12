762213e9c559103e66b3bb36b6f37dfabd06b489
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-04-12 12:48:18 +0530

    creating the chat window

---
+++ src/components/Chat.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="chat container">
+    <h2 class="center teal-text">Ninja Chat</h2>
+    <div class="card"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chat",
+  props: ["name"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -6,6 +6,7 @@
         <form @submit.prevent="enterChat">
           <label for="name">Enter Your name:</label>
           <input type="text" name="name" v-model="name" />
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
           <button class="btn teal">Enter Chat</button>
         </form>
       </div>
@@ -18,12 +19,17 @@ export default {
   name: "Welcome",
   data() {
     return {
-      name: null
+      name: null,
+      feedback: null
     };
   },
   methods: {
     enterChat() {
-      console.log(this.name);
+      if (this.name) {
+        this.$router.push({ name: "Chat", params: { name: this.name } });
+      } else {
+        this.feedback = "You Must Enter a Name to Join";
+      }
     }
   }
 };
